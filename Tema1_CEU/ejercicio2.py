e3bc2965dfbec8f998f3dc608f6414b8b4373fc0
Diegopj22 <113629733+Diegopj22@users.noreply.github.com>
2022-10-02 18:44:48 +0200

    Add files via upload

---
+++ Tema1_CEU/ejercicio2.py
@@ -0,0 +1,5 @@
+numero_magico=12345679
+numero_usuario=int(input("Introduce un número que este entre el 1 y 9 "))
+numero_usuario2=numero_usuario*9
+numero_final=numero_magico*numero_usuario2
+print("el numero mágico es",numero_final)
